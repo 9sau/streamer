599c78eab9d8699c1e3a188d0da7d98720ca6286
saurabh <saurabhjain.kjsce@gmail.com>
2019-04-10 22:57:28 -0500

    changed to class component, added redux form

--- streamy-client/src/components/stream/StreamCreate.jsx
+++ streamy-client/src/components/stream/StreamCreate.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import React from "react";
+import { Field, reduxForm } from "redux-form";
+class StreamCreate extends React.Component {
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>
+  onSubmit(formValues) {
+    console.log(formValues);
+  }
+
+  render() {
+    return (
+      <div>
+        <form
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+          className="ui form"
+        >
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Stream Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter Stream Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default StreamCreate;
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
