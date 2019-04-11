84038afd61e32cf43f2da854e334ad0d042847c4
sj404x <sj404x@us.att.com>
2019-04-11 17:14:31 -0500

    class based stream list component, linked action creator

--- streamy-client/src/components/stream/StreamList.jsx
+++ streamy-client/src/components/stream/StreamList.jsx
@@ -1,7 +1,42 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import { getStreams } from "../../actions";
 
-const StreamList = () => {
-    return <div>StreamList</div>
+class StreamList extends React.Component {
+  componentDidMount() {
+    this.props.getStreams();
+  }
+
+  renderStreams() {
+    return this.props.streams.map(({ id, title, description }) => {
+      return (
+        <div className="ui raised card" key={id}>
+          <div className="content">
+            <div className="header">{title}</div>
+            <div className="description">{description}</div>
+          </div>
+          <div className="extra content">
+            <div className="right floated author">
+              <button class="ui blue tiny basic button">Edit</button>
+              <button class="ui red tiny basic button">Delete</button>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  }
+
+  render() {
+    return <div className="ui four cards">{this.renderStreams()}</div>;
+  }
 }
 
-export default StreamList;
+const mapStateToProps = state => {
+  return {
+    streams: Object.values(state.streams)
+  };
+};
+export default connect(
+  mapStateToProps,
+  { getStreams }
+)(StreamList);
