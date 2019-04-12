9872f5f92a68f55ecf608378fa17d0435b8bb06c
sj404x <sj404x@us.att.com>
2019-04-12 14:17:51 -0500

    changing from Browser Router to Plain Router for custom navigation

--- streamy-client/src/components/App.js
+++ streamy-client/src/components/App.js
@@ -1,23 +1,24 @@
-import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
-import StreamList from "./stream/StreamList";
-import StreamEdit from "./stream/StreamEdit";
-import StreamCreate from "./stream/StreamCreate";
-import StreamWatch from "./stream/StreamWatch";
-import StreamUpdate from "./stream/StreamUpdate";
-import Header from "./Header";
+import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import StreamList from './stream/StreamList';
+import StreamEdit from './stream/StreamEdit';
+import StreamCreate from './stream/StreamCreate';
+import StreamWatch from './stream/StreamWatch';
+import StreamUpdate from './stream/StreamUpdate';
+import Header from './Header';
+import history from '../history';
 
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
+      <Router history={history}>
         <Header />
         <Route path="/" exact component={StreamList} />
         <Route path="/stream/new" exact component={StreamCreate} />
         <Route path="/stream/view" exact component={StreamWatch} />
         <Route path="/stream/update" exact component={StreamUpdate} />
         <Route path="/stream/edit/:id" exact component={StreamEdit} />
-      </BrowserRouter>
+      </Router>
     </div>
   );
 };
