3dbd9abc3dd5b9b5374bed0a082f162606b78052
sj404x <sj404x@us.att.com>
2019-04-12 20:28:45 -0500

    added delete modal on Stream Delete

--- streamy-client/src/components/stream/StreamDelete.jsx
+++ streamy-client/src/components/stream/StreamDelete.jsx
@@ -3,19 +3,35 @@ import { connect } from 'react-redux';
 import { getStream, deleteStream } from '../../actions';
 import Modal from '../Modal';
 
-const StreamDelete = () => {
-  const actions = (
-    <React.Fragment>
-      <div className="ui button negative">Delete</div>
-      <div className="ui button cancel">Cancel</div>
-    </React.Fragment>
-  );
-  return (
-    <Modal title="Stream Delete" content="Are you sure?" actions={actions} />
-  );
-};
+class StreamDelete extends React.Component {
+  componentDidMount() {
+    this.props.getStream(this.props.match.params.id);
+  }
+  renderActions() {
+    return (
+      <React.Fragment>
+        <div className="ui button negative">Delete</div>
+        <div className="ui button cancel">Cancel</div>
+      </React.Fragment>
+    );
+  }
+  render() {
+    return (
+      <Modal
+        title="Stream Delete"
+        content="Are you sure?"
+        actions={this.renderActions()}
+      />
+    );
+  }
+}
 
+const mapStateToProps = (state, ownProps) => {
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
+};
 export default connect(
-  null,
+  mapStateToProps,
   { getStream, deleteStream }
 )(StreamDelete);
